c1d164291e944ad72deb48ef66aae9463f02604a
Sergey Sachik <serega3000@gmail.com>
2015-04-14 19:58:25 +0300

    unused code deleted

--- views/default/plugins/gallery_field/settings.php
+++ views/default/plugins/gallery_field/settings.php
@@ -28,18 +28,3 @@ echo elgg_view('input/select', array(
 	'value' => $vars['entity']->enable_pages,
 ));
 echo '</div>';
-
-
-/*
-// Map width
-$map_width = elgg_view('input/text', array('name' => 'params[map_width]', 'value' => $plugin->map_width));
-$map_width .= "<div class='elgg-subtext'>" . elgg_echo('routes:settings:map_width:how') . "</div>";
-
-echo elgg_view_module("inline", elgg_echo('routes:settings:map_width'), $map_width);	
-
-// Map height
-$map_height = elgg_view('input/text', array('name' => 'params[map_height]', 'value' => $plugin->map_height));
-$map_height .= "<div class='elgg-subtext'>" . elgg_echo('routes:settings:map_height:how') . "</div>";
-
-echo elgg_view_module("inline", elgg_echo('routes:settings:map_height'), $map_height);	        
-	*/
